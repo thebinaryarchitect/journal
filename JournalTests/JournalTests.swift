4aa73c1bb5cbafa446dba44f5274aa02ee2dd3cf
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-14 18:28:21 -0500

    Add license

--- JournalTests/JournalTests.swift
+++ JournalTests/JournalTests.swift
@@ -2,8 +2,8 @@
 //  JournalTests.swift
 //  JournalTests
 //
-//  Created by Xiao Yao on 12/14/15.
-//  Copyright © 2015 Xiao Yao. All rights reserved.
+//  Copyright (C) 2015 Xiao Yao. All Rights Reserved.
+//  See LICENSE.txt for more information.
 //
 
 import XCTest
